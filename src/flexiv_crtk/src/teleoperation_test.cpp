720ec1003da91caf94c69f951741e4b63701bd0b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-07-03 18:03:14 +0800

    Update teleoperation_test.cpp

--- src/flexiv_crtk/src/teleoperation_test.cpp
+++ src/flexiv_crtk/src/teleoperation_test.cpp
@@ -9,23 +9,28 @@
 #include <eigen3/Eigen/Dense>
 #include "ros/ros.h"
 #include "geometry_msgs/TransformStamped.h"
+#include "std_msgs/Bool.h"
 #include "flexiv_crtk/RobotStates_CRTK.h"
 
 // IP of the robot server
 std::string robotIP = "192.168.2.100";
 // IP of the workstation PC running this program
-std::string localIP = "192.168.2.109";
+std::string localIP = "192.168.2.108";
 std::string master_measured_cp_topic = "/MTML/measured_cp";
+std::string master_align_state_topic = "/MTM_align_state";
+std::string master_gripper_closed_totic = "MTML/gripper/closed";
 Eigen::Vector3d master_translation, robot_translation, initial_master_translation, initial_robot_translation, master_translation_variation;
 Eigen::Quaterniond master_quaterion, robot_quaterion, initial_master_quaterion, initial_robot_quaterion;
 Eigen::Matrix4d master_rotation_mat, robot_rotation_mat, initial_master_rotation_mat, initial_robot_rotation_mat;
 double translation_scale = 1;
 Eigen::Matrix3d master_to_robot_base_transform = Eigen::MatrixXd::Identity(3, 3); // happy to be consistent
 bool master_pose_initialized = false;
+bool master_aligned = false;
+bool master_gripper_once_closed = false;
 bool robot_pose_initialized = false;
 
 void masterMsgCallback(const geometry_msgs::TransformStamped& msg) {
-    std::cout << "Successfully receive MTML cp: " << msg.header.seq << '\n';
+    // std::cout << "Successfully receive MTML cp: " << msg.header.seq << '\n';
     master_translation(0) = msg.transform.translation.x;
     master_translation(1) = msg.transform.translation.y;
     master_translation(2) = msg.transform.translation.z;
@@ -42,6 +47,15 @@ void masterMsgCallback(const geometry_msgs::TransformStamped& msg) {
     }
 }
 
+void masterAlignMsgCallback(const std_msgs::Bool& msg) {
+    master_aligned = msg.data;
+}
+
+void masterGripperClosedMsgCallback(const std_msgs::Bool& msg) {
+    if (msg.data == true)
+        master_gripper_once_closed = true;
+}
+
 void robotMsgCallback(const flexiv_crtk::RobotStates_CRTK& msg) {
         robot_translation(0) = msg.measured_cp.position.x;
         robot_translation(1) = msg.measured_cp.position.y;
@@ -52,16 +66,17 @@ void robotMsgCallback(const flexiv_crtk::RobotStates_CRTK& msg) {
         robot_quaterion.y() = msg.measured_cp.orientation.y;
         robot_quaterion.z() = msg.measured_cp.orientation.z;
     if (robot_pose_initialized == false) {
-        master_pose_initialized = true;
+        robot_pose_initialized = true;
         initial_robot_translation = robot_translation;
         initial_robot_quaterion = robot_quaterion;
     }
 }
 
 void transformPoseIntoVec(std::vector<double>& target_pose, Eigen::Vector3d& translation, Eigen::Quaterniond& quaterion) {
-    target_pose[0] = translation(0);
-    target_pose[1] = translation(1);
-    target_pose[2] = translation(2);
+    translation = master_to_robot_base_transform * translation;
+    target_pose[0] = initial_robot_translation(0) + translation(0);
+    target_pose[1] = initial_robot_translation(1) + translation(1);
+    target_pose[2] = initial_robot_translation(2) + translation(2);
 
     target_pose[3] = quaterion.w();
     target_pose[4] = quaterion.x();
@@ -70,7 +85,7 @@ void transformPoseIntoVec(std::vector<double>& target_pose, Eigen::Vector3d& tra
 }
 
 int main(int argc, char** argv) {
-/*     flexiv::Log log;
+    flexiv::Log log;
     flexiv::Robot_CRTK robot(robotIP, localIP, true);
     try {
         if (robot.isFault()) {
@@ -84,11 +99,9 @@ int main(int argc, char** argv) {
             log.info("Fault on robot server is cleared");
         }
 
-        // Enable the robot, make sure the E-stop is released before enabling
         log.info("Enabling robot ...");
         robot.enable();
 
-        // Wait for the robot to become operational
         int secondsWaited = 0;
         while (!robot.isOperational()) {
             std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -103,10 +116,10 @@ int main(int argc, char** argv) {
     } catch (const flexiv::Exception& e) {
         log.error(e.what());
         return 1;
-    } */
+    }
 
     std::string test_flag;
-    std::cout << "Press 1 to start teleoperation.\n";
+    std::cout << "Press 1 to start teleoperation\n";
     getline(std::cin, test_flag);
     
     if (test_flag != "1") {
@@ -116,22 +129,33 @@ int main(int argc, char** argv) {
     
     ros::init(argc, argv, "flexiv_teleoperation_node");
     ros::NodeHandle node_handle;
+    ros::Subscriber mater_align_state_sub = node_handle.subscribe(master_align_state_topic, 100, masterAlignMsgCallback);
+    ros::Subscriber master_gripper_closed_sub = node_handle.subscribe(master_gripper_closed_totic, 100, masterGripperClosedMsgCallback);
     ros::Subscriber master_cp_sub = node_handle.subscribe(master_measured_cp_topic, 100, masterMsgCallback);
     ros::Subscriber robot_cp_sub = node_handle.subscribe("flexiv/robot_states/", 100, robotMsgCallback);
+    ros::Rate loop_rate(100);
     
-    // TO-DO: alignment of master
-
-    //while (master_pose_initialized == false || robot_pose_initialized == false) {
-    //    std::cout << "Wait for completing master and robot pose initialization\n";
-    //}
-    std::cout << "Ready to start teleoperation\n";
-
+    // master alignment
+    while (master_aligned == false) {
+        std::cout << "Waiting for master to complete alignment\n";
+        ros::spinOnce();
+    }
+    
+    while (master_pose_initialized == false || robot_pose_initialized == false) {
+        std::cout << "Wait for completing master and robot pose initialization\n";
+        ros::spinOnce();
+    }
+    while (master_gripper_once_closed == false) {
+        std::cout << "Ready to start teleoperation. Clamp the master finger gripper to start teleoperation\n";
+        ros::spinOnce();
+    }
     while (true) {
         master_translation_variation = translation_scale * (master_translation - initial_master_translation);
         std::vector<double> robot_target_pose(7, 0);
         transformPoseIntoVec(robot_target_pose, master_translation_variation, master_quaterion);
-        ros::spin();
-        // robot.servo_cp(robot_target_pose);
+        robot.interpolate_cp(robot_target_pose);
+        ros::spinOnce();
+        loop_rate.sleep();
     }
     return 0;
 }
